b648b9a991a3325ed1d3abd4ea1f1a54e15be708
samsularifin05 <samsul026@gmail.com>
2024-03-07 14:49:26 +0700

    update main

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,8 @@ import App from "./App.tsx";
 import "./index.css";
 import "aos/dist/aos.css";
 import Aos from "aos";
-import { BrowserRouter } from "react-router-dom";
 Aos.init({ duration: 1200 });
+import { BrowserRouter } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
